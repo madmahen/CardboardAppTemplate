068f30d6136851bbb78aaf96f09393d10fb69b80
Yuya Matsuo <matsuo@eje-c.com>
2014-11-26 14:51:13 +0900

    init commit

---
+++ app/src/main/assets/main.js
@@ -0,0 +1,72 @@
+(function(window) {
+  var app = window.app = new CardboardApp(); // Create global variable app to access from Java code.
+  var scene = app.scene,
+    camera = app.camera,
+    renderer = app.renderer;
+
+  // init scene with sample contents
+  var light = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.4);
+  scene.add(light);
+
+  var texture = THREE.ImageUtils.loadTexture(
+    'textures/patterns/checker.png'
+  );
+  texture.wrapS = THREE.RepeatWrapping;
+  texture.wrapT = THREE.RepeatWrapping;
+  texture.repeat = new THREE.Vector2(100, 100);
+  texture.anisotropy = renderer.getMaxAnisotropy();
+
+  // Create floor
+  var floor = new THREE.Mesh(
+    new THREE.PlaneBufferGeometry(1000, 1000),
+    new THREE.MeshPhongMaterial({
+      color: 0xffffff,
+      specular: 0x666666,
+      shininess: 20,
+      shading: THREE.FlatShading,
+      map: texture
+    })
+  );
+  floor.rotation.x = -Math.PI / 2;
+  scene.add(floor);
+
+  // Create cube
+  var cube = new THREE.Mesh(new THREE.BoxGeometry(5, 5, 5), new THREE.MeshBasicMaterial({
+    color: 0xff9900
+  }));
+
+  scene.add(cube);
+
+  // Move camera to see a cube
+  camera.position.y = 10;
+  camera.position.z = -10;
+
+  // Add update callback receiver
+  // e.detail is CardboardApp.State object.
+  // e.detail.dt is a result of THREE.Clock.getDelta() for each frames.
+  // e.detail.touching is true while user is touching to screen (with VR kit's button).
+  app.on('update', function(e) {
+    var state = e.detail;
+
+    // Move forward while touching
+    if (state.touching) {
+      var vec = new THREE.Vector3(0, 0, -1);
+      vec.applyQuaternion(camera.quaternion);
+      vec.y = 0; // now vec is forward vector.
+
+      vec.multiplyScalar(10 * state.dt);
+      camera.position.add(vec);
+    }
+  });
+
+  // And be able to add more callback(s)
+  app.on('update', function(e) {
+    cube.rotation.y += 0.5 * e.detail.dt;
+  });
+
+  app.on('click', function() {
+    app.requestFullscreen();
+  }).on('cardboardTrigger', function(e) {
+    console.log(e);
+  });
+})(window);
---
+++ app/src/main/java/cardboardtemplate/eje_c/com/cardboardtemplate/MainActivity.java
@@ -0,0 +1,36 @@
+package cardboardtemplate.eje_c.com.cardboardtemplate;
+
+import android.os.Bundle;
+import android.webkit.ValueCallback;
+
+import com.google.vrtoolkit.cardboard.CardboardActivity;
+
+import org.xwalk.core.XWalkPreferences;
+import org.xwalk.core.XWalkView;
+
+
+public class MainActivity extends CardboardActivity {
+    private XWalkView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (BuildConfig.DEBUG) {
+            XWalkPreferences.setValue(XWalkPreferences.REMOTE_DEBUGGING, true);
+        }
+
+        webView = (XWalkView) findViewById(R.id.webView);
+        webView.loadAppFromManifest("file:///android_asset/manifest.json", null);
+    }
+
+    @Override
+    public void onCardboardTrigger() {
+        webView.evaluateJavascript("app.emit('cardboardTrigger');", new ValueCallback<String>() {
+            @Override
+            public void onReceiveValue(String value) {
+            }
+        });
+    }
+}
