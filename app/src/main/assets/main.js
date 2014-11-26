101baf231484825f121a574b366e91ba9874f43b
Yuya <matsuo@eje-c.com>
2014-11-26 16:34:21 +0900

    update sample

--- app/src/main/assets/main.js
+++ app/src/main/assets/main.js
@@ -1,44 +1,12 @@
 (function(window) {
-  var app = window.app = new CardboardApp(); // Create global variable app to access from Java code.
+  // Create global variable app to access from Java code.
+  var app = window.app = new CardboardApp();
+
   var scene = app.scene,
     camera = app.camera,
     renderer = app.renderer;
 
-  // init scene with sample contents
-  var light = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.4);
-  scene.add(light);
-
-  var texture = THREE.ImageUtils.loadTexture(
-    'textures/patterns/checker.png'
-  );
-  texture.wrapS = THREE.RepeatWrapping;
-  texture.wrapT = THREE.RepeatWrapping;
-  texture.repeat = new THREE.Vector2(100, 100);
-  texture.anisotropy = renderer.getMaxAnisotropy();
-
-  // Create floor
-  var floor = new THREE.Mesh(
-    new THREE.PlaneBufferGeometry(1000, 1000),
-    new THREE.MeshPhongMaterial({
-      color: 0xffffff,
-      specular: 0x666666,
-      shininess: 20,
-      shading: THREE.FlatShading,
-      map: texture
-    })
-  );
-  floor.rotation.x = -Math.PI / 2;
-  scene.add(floor);
-
-  // Create cube
-  var cube = new THREE.Mesh(new THREE.BoxGeometry(5, 5, 5), new THREE.MeshBasicMaterial({
-    color: 0xff9900
-  }));
-  scene.add(cube);
-
-  // Move camera to see a cube
-  camera.position.y = 10;
-  camera.position.z = -10;
+  initScene(scene);
 
   var walking = false;
   app.on('click', function() {
@@ -64,4 +32,42 @@
   app.on('update', function(e) {
     cube.rotation.y += 0.5 * e.detail.dt;
   });
+
+  function initScene(scene) {
+    // init scene with sample contents
+    var light = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.4);
+    scene.add(light);
+
+    var texture = THREE.ImageUtils.loadTexture(
+      'textures/patterns/checker.png'
+    );
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat = new THREE.Vector2(100, 100);
+    texture.anisotropy = renderer.getMaxAnisotropy();
+
+    // Create floor
+    var floor = new THREE.Mesh(
+      new THREE.PlaneBufferGeometry(1000, 1000),
+      new THREE.MeshPhongMaterial({
+        color: 0xffffff,
+        specular: 0x666666,
+        shininess: 20,
+        shading: THREE.FlatShading,
+        map: texture
+      })
+    );
+    floor.rotation.x = -Math.PI / 2;
+    scene.add(floor);
+
+    // Create cube
+    var cube = new THREE.Mesh(new THREE.BoxGeometry(5, 5, 5), new THREE.MeshBasicMaterial({
+      color: 0xff9900
+    }));
+    scene.add(cube);
+
+    // Move camera to see a cube
+    camera.position.y = 10;
+    camera.position.z = -10;
+  }
 })(window);
